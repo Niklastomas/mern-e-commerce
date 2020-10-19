2ad1dd190a0301969a827fdb03b5c41eb32028aa
Niklas Tomas <niklas.thomas@hotmail.com>
2020-10-19 11:43:39 +0200

    added HomeScreen, Product, Rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main className='py-3'>
         <Container>
           <h1>Welcome To E-Shop</h1>
+          <HomeScreen />
         </Container>
       </main>
 
