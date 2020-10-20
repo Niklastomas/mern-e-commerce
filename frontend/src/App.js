4ccd28819ebf76650af256c1315448fc4441bbbb
Niklas Tomas <niklas.thomas@hotmail.com>
2020-10-20 15:20:12 +0200

    added React Router, ProductScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome To E-Shop</h1>
-          <HomeScreen />
+          <Route exact path='/' component={HomeScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
 
       <Footer />
-    </>
+    </Router>
   );
 }
 
