209dff56e14050cf49d11df82b7e9d64e6297240
Niklas Tomas <niklas.thomas@hotmail.com>
2020-10-23 23:27:45 +0200

    added authMiddleware, jsonwebtoken, update userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,8 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
-// @desc    Auth yser & get token
+// @desc    Auth user & get token
 // @route   POST /api/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
@@ -15,11 +16,62 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401).json({ message: 'Invalid email or password' });
   }
 });
 
-export { authUser };
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404).json({ message: 'User not found' });
+  }
+});
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400).json({ message: 'User already exists' });
+  } else {
+    const user = await User.create(
+      {
+        name,
+        email,
+        password,
+      },
+      (err, user) => {
+        if (err) {
+          res.status(400).json({ message: 'Invalid user data' });
+        } else {
+          res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+          });
+        }
+      }
+    );
+  }
+});
+
+export { authUser, getUserProfile, registerUser };
